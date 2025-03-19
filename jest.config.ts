211d71c915b55811b2051a69208e0fdcacd451d0
in-ch <sxin2949@naver.com>
2025-03-19 21:50:44 +0900

    chore: project init

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+const config = {
+  preset: "ts-jest",
+  testMatch: ["<rootDir>/**/__tests__/**/*.test.[jt]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  testEnvironment: "jsdom",
+};
+
+module.exports = config;
