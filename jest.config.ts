aa0ed7a0ed29c36058814f7d2d8743680afe52f7
in-ch <sxin2949@naver.com>
2025-03-22 22:22:33 +0900

    build: add test library

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,10 @@
-const config = {
-  preset: "ts-jest",
-  testMatch: ["<rootDir>/**/__tests__/**/*.test.[jt]s?(x)"],
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  testEnvironment: "jsdom",
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testMatch: ['<rootDir>/**/__tests__/**/*.test.[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testEnvironment: 'jest-environment-jsdom',
 };
 
 module.exports = config;
