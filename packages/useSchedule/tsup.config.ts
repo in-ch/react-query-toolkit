0e9d775f5306bc062052ab341bb04b9ecb2eef50
in-ch <sxin2949@naver.com>
2025-03-24 17:08:45 +0900

    build: tsup build setting

---
+++ packages/useSchedule/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ['./index.ts'],
+  external: ['useSchedule/package.json'],
+  format: ['esm', 'cjs'],
+  sourcemap: true,
+  minify: true,
+  target: 'esnext',
+  outDir: 'dist',
+});
