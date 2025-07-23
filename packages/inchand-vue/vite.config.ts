e5eef2369fdf0ddfbfcc78c13a1ed1f818fc43a3
in-ch <sxin2949@naver.com>
2025-07-23 17:16:01 +0900

    feat(inchand-vue): add test packages

--- packages/inchand-vue/vite.config.ts
+++ packages/inchand-vue/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import path from 'path';
-import react from '@vitejs/plugin-react';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vue() as any],
   test: {
     environment: 'jsdom',
     globals: true,
